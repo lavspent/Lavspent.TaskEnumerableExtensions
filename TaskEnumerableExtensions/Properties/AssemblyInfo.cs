f2d5ddf4c8ddeaa088d77116fa62e31d4ac09caa
Petter Labråten <petter.labraaten@labraaten.com>
2017-04-01 03:41:15 +0200

    Added nuget-stuff.

--- TaskEnumerableExtensions/Properties/AssemblyInfo.cs
+++ TaskEnumerableExtensions/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Lavspent.TaskEnumerableExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extension methods for Task<IEnumerable>")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Petter Labråten")]
 [assembly: AssemblyProduct("TaskEnumerableExtensions")]
-[assembly: AssemblyCopyright("Copyright © Petter Labråten 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Petter Labråten")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
